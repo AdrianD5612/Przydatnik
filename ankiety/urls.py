300298f8f5edd397d96f39b50480cd097823e976
AdrianD5612 <adriand5612@gmail.com>
2023-01-30 13:02:39 +0100

    Napisane testy

---
+++ ankiety/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'ankiety'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/wyniki/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/glos/', views.vote, name='vote'),
+]
