9aec79febc41b7e1549e8c56d16f2934ba553440
AdrianD5612 <adriand5612@gmail.com>
2023-01-30 18:36:49 +0100

    rozbudowa administracji

--- ankiety/models.py
+++ ankiety/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 import datetime
 
 class Question(models.Model):
@@ -7,6 +8,14 @@ class Question(models.Model):
     pub_date = models.DateTimeField('data opublikowania')
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
--- ankiety/urls.py
+++ ankiety/urls.py
@@ -6,6 +6,6 @@ app_name = 'ankiety'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/wyniki/', views.ResultsView.as_view(), name='results'),
+    path('<int:pk>/wyniki/', views.ResultsView.as_view(), name='wyniki'),
     path('<int:question_id>/glos/', views.vote, name='vote'),
 ]
