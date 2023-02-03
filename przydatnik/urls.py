85b5d1162ababd0d70237dfac6a612b876e1984e
AdrianD5612 <adriand5612@gmail.com>
2023-02-03 19:20:56 +0100

    Moduł Wydatki z samouczka+sporo zmian własnych

--- przydatnik/urls.py
+++ przydatnik/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('ankiety/', include('ankiety.urls')),
     path('admin/', admin.site.urls),
+    path('',include('wydatki.urls'))
 ]
--- wydatki/views.py
+++ wydatki/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect,HttpResponse
+from .models import ExpenseInfo
+from django.contrib.auth import logout,login,authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.db.models import Sum
+import matplotlib.pyplot as plt
+import numpy as np
+from django.db.models import Q
 # Create your views here.
+
+
+def index(request):
+    expense_items = ExpenseInfo.objects.filter(user_expense=request.user).order_by('-date_added')
+    try:
+        budget_total = ExpenseInfo.objects.filter(user_expense=request.user).aggregate(budget=Sum('cost',filter=Q(cost__gt=0)))
+        expense_total = ExpenseInfo.objects.filter(user_expense=request.user).aggregate(expenses=Sum('cost',filter=Q(cost__lt=0)))
+        fig,ax=plt.subplots()
+        ax.bar(['Expenses','Budget'], [abs(expense_total['expenses']),budget_total['budget']],color=['red','green'])
+        ax.set_title('Your total expenses vs total budget')
+        plt.savefig('wydatki/static/wydatki/expense.jpg')
+    except TypeError:
+        print('No data.')
+    if expense_total['expenses']:
+        context = {'expense_items':expense_items,'budget':budget_total['budget'],'expenses':abs(expense_total['expenses'])}
+    else:   #naprawia błąd w przypadku braku wpisów
+        context = {'expense_items':expense_items,'budget':budget_total['budget'],'expenses':(expense_total['expenses'])}
+    return render(request,'wydatki/index.html',context=context)
+
+def add_item(request):
+    name = request.POST['expense_name']
+    expense_cost = request.POST['cost']
+    expense_date = request.POST['expense_date']
+    ExpenseInfo.objects.create(expense_name=name,cost=expense_cost,date_added=expense_date,user_expense=request.user)
+    budget_total = ExpenseInfo.objects.filter(user_expense=request.user).aggregate(budget=Sum('cost',filter=Q(cost__gt=0)))
+    expense_total = ExpenseInfo.objects.filter(user_expense=request.user).aggregate(expenses=Sum('cost',filter=Q(cost__lt=0)))
+    fig,ax=plt.subplots()
+    if expense_total['expenses']:
+        ax.bar(['Expenses','Budget'], [abs(expense_total['expenses']),budget_total['budget']],color=['red','green'])
+    else:
+        ax.bar(['Expenses','Budget'], [expense_total['expenses'],budget_total['budget']],color=['red','green'])
+    ax.set_title('Your total expenses vs. total budget')
+    plt.savefig('wydatki/static/wydatki/expense.jpg')
+    return HttpResponseRedirect('app')
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
+def sign_up(request):
+    errors=''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            login(request,user)
+            return HttpResponseRedirect('app')
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+                errors+=msg
+            return render(request, 'wydatki/error.html', {
+            'error_message': errors ,
+             })
+    else:
+        form = UserCreationForm
+        return render(request,'wydatki/sign_up.html',{'form':form})
