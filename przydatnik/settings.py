5f7b8f4d2c6e6f4dc7589d56a810622f6e3f92a8
AdrianD5612 <adriand5612@gmail.com>
2023-02-10 19:29:05 +0100

    Grafika bez otwierania nowego okna

--- przydatnik/settings.py
+++ przydatnik/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut aw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.2.44', '127.0.0.1', ]
 
 
 # Application definition
