fe75e0ff6ce23b503ed85e8e6aa233dd3245e7cb
AdrianD5612 <113292537+AdrianD5612@users.noreply.github.com>
2023-03-12 16:07:22 +0100

    Edycja i usuwanie notatek przez użytkownika

--- notatki/urls.py
+++ notatki/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('',views.index),
     path('theme', views.theme),
     path('add_note',views.add_note,name='add note'),
+    path('delete_note/<note_id>',views.delete_note,name='delete note'),
+    path('edit_note/<note_id>',views.edit_note,name='edit note'),
+    path('save_changes/<note_id>',views.save_changes, name='save changes'),
 ]
--- notatki/views.py
+++ notatki/views.py
@@ -6,8 +6,6 @@ from django.db.models import Q
 from notatki.forms import NotatkaForm 
 import datetime 
 
-# TODO:edycja notatek
-
 def index(request): #generowanie strony głównej notatek
     try:
         note_list = Notatka.objects.filter(Q(author=request.user) | Q(publicnote=True)).order_by('-date_added') #Q umożliwia łączone wyrażenie typu OR
@@ -34,6 +32,35 @@ def add_note(request):  #dodawanie notatki
             'form': form
         }
     return render(request, 'index.html', context)
+
+def delete_note(request,note_id=None):  #usuwanie notatki
+    note_to_delete=Notatka.objects.get(id=note_id)
+    note_to_delete.delete()
+    return HttpResponseRedirect('/notatki')
+
+def edit_note(request,note_id=None):  #edytowanie notatki
+    note_to_edit = Notatka.objects.filter(id=note_id)
+    note_to_edit=note_to_edit[0]    #jest tylko jedna notatka więc zamieniam Queryset na pojedyńczy wpis
+    mode='dark' #domyślny motyw=ciemny
+    if Theme.objects.filter(user_theme=request.user).exists():  #jeśli użytkownik już wybierał motyw
+        mode= Theme.objects.get(user_theme=request.user).mode
+    context = {'note_to_edit':note_to_edit, 'mode':mode}
+    return render(request,'edit.html',context=context)
+
+def save_changes(request,note_id=None): #zapisanie zmian w edytowanej notatce
+    if request.method == 'POST':
+        updated_note = Notatka.objects.get(id=note_id)
+        updated_note.title = request.POST.get('title')
+        updated_note.publicnote = request.POST.get('publicnote')
+        # checkbox wymaga przetworzenia na boolean
+        if updated_note.publicnote=='on':
+            updated_note.publicnote=True
+        else:
+            updated_note.publicnote=False
+        updated_note.content = request.POST.get('content')
+        print(updated_note.publicnote)
+        updated_note.save()
+    return redirect('/notatki')
 
 def theme(request): ##wybranie motywu
     mode=request.GET.get('mode')
