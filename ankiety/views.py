5f3e814374e9b0f4a9ba7c214b83779d525b262b
AdrianD5612 <113292537+AdrianD5612@users.noreply.github.com>
2023-05-08 18:44:08 +0200

    Ankiety: przebudowa Index&Detail

--- ankiety/views.py
+++ ankiety/views.py
@@ -1,33 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from .models import Choice, Question
+from wydatki.models import Theme
 
+def index(request):
+    recents=Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
+    mode='dark' #domyślny motyw=ciemny
+    if Theme.objects.filter(user_theme=request.user).exists():  #jeśli użytkownik już wybierał motyw
+                mode= Theme.objects.get(user_theme=request.user).mode
+    context={'latest_question_list':recents, 'mode':mode}
+    return render(request,'ankiety/index.html',context=context)
 
-
-class IndexView(generic.ListView):
-    template_name = 'ankiety/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        """
-        Wyświetla 5 ostatnich ankiet które mają przeszłą date publikacji
-        """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
-
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'ankiety/detail.html'
-    def get_queryset(self):
-        """
-        Wyświetla wszystkie ankiety które mają przeszłą date publikacji
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+def detail(request, pk):
+    details=Choice.objects.filter(question_id=pk)
+    quest=Question.objects.filter(id=pk)
+    quest=quest[0]
+    mode='dark' #domyślny motyw=ciemny
+    if Theme.objects.filter(user_theme=request.user).exists():  #jeśli użytkownik już wybierał motyw
+                mode= Theme.objects.get(user_theme=request.user).mode
+    context={'details':details, 'quest':quest, 'mode':mode}
+    return render(request,'ankiety/detail.html',context=context)
 
 
 class ResultsView(generic.DetailView):
@@ -49,3 +44,26 @@ def vote(request, question_id):
         selected_choice.save()
         # Przekierowanie po wysłaniu POST
         return HttpResponseRedirect(reverse('ankiety:wyniki', args=(question.id,)))
+    
+def theme(request): ##wybranie motywu
+    mode=request.GET.get('mode')
+
+    if mode=='dark':
+        if Theme.objects.filter(user_theme=request.user).exists():
+            user_saving= Theme.objects.get(user_theme=request.user)
+            user_saving.user_theme=request.user
+            user_saving.mode='dark'
+            user_saving.save()
+        else:
+            user2=Theme(user_theme=request.user, mode='dark')
+            user2.save()
+    elif mode=='white':
+        if Theme.objects.filter(user_theme=request.user).exists():
+            user_saving= Theme.objects.get(user_theme=request.user)
+            user_saving.user_theme=request.user
+            user_saving.mode='white'
+            user_saving.save()
+        else:
+            user2=Theme(user_theme=request.user, mode='white')
+            user2.save()
+    return redirect('/ankiety')
